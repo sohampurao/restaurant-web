f69111eaaf70165e2a8ef9cc3c469e5c2d47b979
Soham Purao <94833514+Soham-Purao-12@users.noreply.github.com>
2022-06-24 00:53:30 +0530

    onload event added to the on reveal function

--- js/script.js
+++ js/script.js
@@ -9,23 +9,29 @@ function scrollUP() {
 window.addEventListener('scroll', scrollUP)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+window.addEventListener('load', revealOnScroll)
+
+function revealOnScroll() {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
     delay: 400,
 })
-
+// for home page
 sr.reveal(`#sp-container-m1`, {origin: 'left'})
 sr.reveal(`#sp-container-m2`, {origin: 'top'})
 sr.reveal(`#sp-container-m3`, {origin: 'right'})
-// sr.reveal(`#sp-container-m1, #sp-container-m2, #sp-container-m3`, {reset: 'true'})
-sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {delay: 500})
-// sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {reset: 'true'})
+
+sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {duration: 3000})
+sr.reveal(`#sp-container-m1, #sp-container-m2, #sp-container-m3`, {duration: 3000})
+
+sr.reveal(`#sp-container-m1, #sp-container-m2, #sp-container-m3`, {reset: 'true'})
+sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {reset: 'true'})
+
 sr.reveal(`.menu-item-left .menu-item-container`, {origin: 'left'})
 sr.reveal(`.menu-item-left .menu-item-container`, {reset: 'true'})
 sr.reveal(`.menu-item-right .menu-item-container`, {origin: 'right'})
 sr.reveal(`.menu-item-right .menu-item-container`, {reset: 'true'})
-
-
-// sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+}
