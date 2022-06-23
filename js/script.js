aa3459e343f44307dfec0beec47b6aa00d00d247
Soham Purao <94833514+Soham-Purao-12@users.noreply.github.com>
2022-06-23 23:40:55 +0530

    reveal on scroll animations added

--- js/script.js
+++ js/script.js
@@ -14,15 +14,14 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2500,
     delay: 400,
-    // reset: true
 })
 
 sr.reveal(`#sp-container-m1`, {origin: 'left'})
 sr.reveal(`#sp-container-m2`, {origin: 'top'})
 sr.reveal(`#sp-container-m3`, {origin: 'right'})
-sr.reveal(`#sp-container-m1, #sp-container-m2, #sp-container-m3`, {reset: 'true'})
+// sr.reveal(`#sp-container-m1, #sp-container-m2, #sp-container-m3`, {reset: 'true'})
 sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {delay: 500})
-sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {reset: 'true'})
+// sr.reveal(`.home-card-1, .home-card-2, .home-card-3`, {reset: 'true'})
 sr.reveal(`.menu-item-left .menu-item-container`, {origin: 'left'})
 sr.reveal(`.menu-item-right .menu-item-container`, {origin: 'right'})
 
